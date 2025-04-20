d04c00c14f4e462cbc0ecf79339656663ec4436f
Mohsen <mr.marsxan@gmail.com>
2025-04-20 23:18:04 +0330

    Initial Commit

---
+++ DynamicNavigation/DynamicNavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  DynamicNavigationApp.swift
+//  DynamicNavigation
+//
+//  Created by Mohsen on 4/20/25.
+//
+
+import SwiftUI
+
+@main
+struct DynamicNavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DynamicNavigationTests/DynamicNavigationTests.swift
@@ -0,0 +1,17 @@
+//
+//  DynamicNavigationTests.swift
+//  DynamicNavigationTests
+//
+//  Created by Mohsen on 4/20/25.
+//
+
+import Testing
+@testable import DynamicNavigation
+
+struct DynamicNavigationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
